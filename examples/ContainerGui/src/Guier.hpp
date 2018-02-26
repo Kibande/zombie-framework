b9be097c82ab7422ce66aba57e1d1cdcd4597186
minexew <minexew@gmail.com>
2018-02-26 21:00:43 +0100

    Add examples/ContainerGui

---
+++ examples/ContainerGui/src/Guier.hpp
@@ -0,0 +1,164 @@
+#ifndef Guier_hpp
+#define Guier_hpp
+
+#include <gameui/gameui.hpp>
+
+#include <framework/utility/essentials.hpp>
+#include <framework/utility/params.hpp>
+#include <framework/utility/util.hpp>
+
+#include <optional>
+#include <string>
+#include <vector>
+
+namespace guier {
+    struct Guier_FontBuilder {
+        auto Path(std::string&& path_) { this->path_ = move(path_); return *this; }
+        auto Size(int size_) { this->size_ = size_; return *this; }
+
+        std::optional<std::string> path_;
+        std::optional<int> size_;
+    };
+
+    struct _GuiContainerState {
+        gameui::WidgetContainer* widget;
+        bool saved = false;
+
+        std::vector<_GuiContainerState> childStates;
+
+        template <typename Class>
+        Class* Self() { return static_cast<Class*>(widget); }
+    };
+
+    struct Guier;
+    struct Guier_TableLayout;
+
+    struct Guier_Button {
+        Guier_Button(gameui::Button* widget) : widget(widget) {}
+
+        bool WasClicked() { return widget->WasClicked(); }
+
+        gameui::Button* widget;
+    };
+
+    struct Guier_Container {
+        Guier_Container(Guier& g, _GuiContainerState& state, bool newlyCreated) : g(g), state(state), newlyCreated(newlyCreated) {}
+
+        Guier_TableLayout TableLayout(int numColumns);
+
+        Guier_Button Button(const char* text);
+        void StaticText(const char* text, const Guier_FontBuilder& font);
+
+        bool IsSaved() const { return state.saved; }
+        void Save() { state.saved = true; }
+        void Unsave() { state.saved = false; }
+
+        Guier& g;
+        _GuiContainerState& state;
+        bool newlyCreated;
+
+        size_t nextContainerIndex = 0;
+        size_t nextWidgetIndex = 0;
+    };
+
+    struct Guier_TableLayout : Guier_Container {
+        using Guier_Container::Guier_Container;
+
+        void SetColumnGrowable(size_t column, bool growable) { if (newlyCreated) this->state.Self<gameui::TableLayout>()->SetColumnGrowable(column, growable); }
+        void SetRowGrowable(size_t row, bool growable) { if (newlyCreated) this->state.Self<gameui::TableLayout>()->SetRowGrowable(row, growable); }
+    };
+
+    struct Guier_Ui : Guier_Container {
+        using Guier_Container::Guier_Container;
+    };
+
+    struct Guier {
+        Guier(zfw::ISystem* sys, gameui::UIThemer* themer, gameui::UIContainer* container) : sys(sys), themer(themer), container(container) {}
+
+        Guier_Ui Ui() {
+            if (state) {
+                if (!state->saved) {
+                    state->widget->RemoveAll();
+                    state->childStates.clear();
+                }
+
+                return Guier_Ui(*this, *state, false);
+            }
+
+            state = {container};
+            return Guier_Ui(*this, *state, true);
+        }
+
+        intptr_t GetFont(const Guier_FontBuilder& fontBuilder) {
+            zombie_assert(fontBuilder.path_ && fontBuilder.size_);
+
+            // FIXME: will use slow lookup
+            auto x = zfw::Params::BuildAlloc(2, "path", fontBuilder.path_->c_str(), "size", sprintf_15("%d", *fontBuilder.size_));
+            auto id = themer->GetFontId(x);
+            free(x);
+            return id;
+        }
+
+        zfw::ISystem* sys;
+        gameui::UIThemer* themer;
+        gameui::UIContainer* container;
+
+        std::optional<_GuiContainerState> state;
+    };
+
+    Guier_Button Guier_Container::Button(const char* text) {
+        size_t idx = nextContainerIndex + nextWidgetIndex;      // TODO: explain this!!
+        nextWidgetIndex++;
+
+        auto& l = state.widget->GetWidgetList();
+
+        if (idx < l.getLength()) {
+            return static_cast<gameui::Button*>(l[idx]);
+        }
+        else {
+            auto widget = new gameui::Button(g.themer, text);
+            widget->AcquireResources();
+            state.widget->Add(widget);
+            return widget;
+        }
+    }
+
+    void Guier_Container::StaticText(const char* text, const Guier_FontBuilder& font) {
+        size_t idx = nextContainerIndex + nextWidgetIndex;      // TODO: explain this!!
+        nextWidgetIndex++;
+
+        auto& l = state.widget->GetWidgetList();
+
+        if (idx < l.getLength()) {
+            return;
+        }
+        else {
+            auto widget = new gameui::StaticText(g.themer, text, g.GetFont(font));
+            widget->AcquireResources();
+            state.widget->Add(widget);
+        }
+    }
+
+    Guier_TableLayout Guier_Container::TableLayout(int numColumns) {
+        size_t pos = nextContainerIndex;
+        nextContainerIndex++;
+
+        if (pos < state.childStates.size()) {
+            // exists, but contents might not be saved
+            if (!state.childStates[pos].saved) {
+                state.childStates[pos].widget->RemoveAll();
+                state.childStates[pos].childStates.clear();
+            }
+
+            return Guier_TableLayout {g, state.childStates[pos], false};
+        }
+
+        auto tableLayout = new gameui::TableLayout(numColumns);
+        tableLayout->SetOnlineUpdate(false);
+        state.widget->Add(tableLayout);
+        state.childStates.push_back(_GuiContainerState {tableLayout});
+        return Guier_TableLayout {g, state.childStates.back(), true};
+    }
+}
+
+#endif
---
+++ examples/ContainerGui/src/main.cpp
@@ -0,0 +1,107 @@
+#include <Guier.hpp>
+
+#include <Container/ContainerApp.hpp>
+#include <Container/ContainerScene.hpp>
+
+#include <framework/event.hpp>
+#include <framework/messagequeue.hpp>
+#include <framework/system.hpp>
+
+namespace example {
+    using Container::ContainerApp;
+    using Container::ContainerScene;
+    using namespace zfw;
+    using namespace guier;
+
+    class ExampleScene : public ContainerScene {
+    public:
+        ExampleScene(ContainerApp* app) : ContainerScene(app, kUseUI) {}
+
+        virtual bool PreBindDependencies() override {
+            this->SetClearColor(Float4(0.2f, 0.2f, 0.3f, 1.0f));
+
+            auto ui = this->GetUILayer()->GetUIContainer();
+
+            /*auto font = uiThemer->PreregisterFont(nullptr, "path=ContainerAssets/font/DejaVuSans.ttf,size=120");
+
+            auto layout = new gameui::TableLayout(1);
+            layout->SetColumnGrowable(0, true);
+            layout->SetRowGrowable(0, true);
+            layout->Add(new gameui::StaticText(uiThemer.get(), "Hello World!", font));
+            ui->Add(layout);*/
+
+            g = std::make_unique<Guier>(app->GetSystem(), uiThemer.get(), ui);
+
+            return true;
+        }
+
+        void OnFrame(double delta) override {
+            ContainerScene::OnFrame(delta);
+
+            if (g)
+                gui(*g);
+
+            if (sceneLayerUI)
+                sceneLayerUI->GetUIContainer()->Layout();
+        }
+
+        void gui(Guier& g);
+
+        std::unique_ptr<Guier> g;
+
+        int counter = 0;
+        std::vector<int> numbers;
+    };
+
+    class MyContainerApp : public ContainerApp {
+    public:
+        MyContainerApp() : ContainerApp("ContainerGui") {
+        }
+
+        std::shared_ptr<zfw::IScene> CreateInitialScene() {
+            return std::make_shared<ExampleScene>(this);
+        }
+    };
+
+    void ExampleScene::gui(Guier& g) {
+        auto ui = g.Ui();
+            auto font = Guier_FontBuilder().Path("ContainerAssets/font/DejaVuSans.ttf").Size(120);
+
+            auto layout = ui.TableLayout(1);
+            layout.SetColumnGrowable(0, true);
+            layout.SetRowGrowable(0, true);
+            layout.Save();
+
+                layout.StaticText("Hello World!", font);
+
+                for (size_t i = 0; i < numbers.size(); i++) {
+                    auto button = layout.Button(sprintf_255("%d (click to remove!)", numbers[i]));
+
+                    if (button.WasClicked()) {
+                        numbers.erase(numbers.begin() + i);
+                        i--;
+                        layout.Unsave();
+                        continue;
+                    }
+                }
+
+                auto increaseCounter = layout.Button(sprintf_255("Increase counter (%d)", this->counter));
+                auto addRandom = layout.Button("Add number");
+
+        ui.Save();
+
+        if (increaseCounter.WasClicked()) {
+            this->counter++;
+            layout.Unsave();
+        }
+
+        if (addRandom.WasClicked()) {
+            this->numbers.push_back(rand());
+            layout.Unsave();
+        }
+    }
+}
+
+int main(int argc, char** argv) {
+    return Container::runContainerApp<example::MyContainerApp>(argc, argv);
+}
